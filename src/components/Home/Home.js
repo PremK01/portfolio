9cb3464ca36c9aef5d5a3f84cc0b5db56dd3e7a7
PremPrasad <103207259+PremK01@users.noreply.github.com>
2025-01-04 14:27:35 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
+//home.js
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
+
 
 function Home() {
   return (
@@ -10,8 +12,8 @@ function Home() {
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
-          <Row className="home-row">
-            <Col md={6} className="home-header">
+          <Row>
+            <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
@@ -23,9 +25,11 @@ function Home() {
                 I'M
                 <strong className="main-name"> PREM PRASAD K</strong>
               </h1>
+
+              
             </Col>
 
-            <Col md={6} className="home-img-col">
+            <Col md={5} style={{ paddingBottom: 20 }}>
               <img
                 src={homeLogo}
                 alt="home pic"
