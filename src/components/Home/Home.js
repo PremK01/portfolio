6993840af26f6dfe9036794c4b84188bd49833d0
PremPrasad <103207259+PremK01@users.noreply.github.com>
2025-01-04 15:00:00 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,14 @@
+//home.js
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
-import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import Home2 from "./Home2";
+
 
 function Home() {
   return (
     <section>
-      {/* Home Section */}
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
@@ -31,6 +25,8 @@ function Home() {
                 I'M
                 <strong className="main-name"> PREM PRASAD K</strong>
               </h1>
+
+              
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
@@ -44,85 +40,7 @@ function Home() {
           </Row>
         </Container>
       </Container>
-
-      {/* Home2 Section (About Me) */}
-      <Container fluid className="home-about-section" id="about">
-        <Container>
-          <Row>
-            <Col md={8} className="home-about-description">
-              <h1 style={{ fontSize: "2.6em" }}>
-                LET ME <span className="purple"> INTRODUCE </span> MYSELF
-              </h1>
-              <p className="home-about-body">
-                I’ve developed a strong passion for programming and have made significant progress 
-                along the way. 
-                <br />
-                <br />I am skilled in both
-                <i>
-                  <b className="purple"> front-end and back-end technologies </b>
-                </i>
-                <br />
-                <br />
-                I’m particularly interested in building &nbsp;
-                <i>
-                  <b className="purple">innovative Web Technologies and Products </b> and
-                  also in areas related to{" "}
-                  <b className="purple">
-                  Machine Learning.
-                  </b>
-                </i>
-                <br />
-                <br />
-                I enjoy working on applications using <b className="purple">Node.js, Express,</b> and
-                <i>
-                  <b className="purple">
-                    {" "}
-                    Modern Javascript Library and Frameworks
-                  </b>
-                </i>
-                &nbsp; like
-                <i>
-                  <b className="purple"> React.js</b>
-                </i>
-              </p>
-            </Col>
-            <Col md={4} className="myAvtar">
-              <Tilt>
-                <img src={myImg} className="img-fluid" alt="avatar" />
-              </Tilt>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12} className="home-about-social">
-              <p>
-                Feel free to <span className="purple">connect </span>with me
-              </p>
-              <ul className="home-about-social-links">
-                <li className="social-icons">
-                  <a
-                    href="https://github.com/PremK01"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <AiFillGithub />
-                  </a>
-                </li>
-                <li className="social-icons">
-                  <a
-                    href="https://www.linkedin.com/in/premk01/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="icon-colour  home-social-icons"
-                  >
-                    <FaLinkedinIn />
-                  </a>
-                </li>
-              </ul>
-            </Col>
-          </Row>
-        </Container>
-      </Container>
+      <Home2 />
     </section>
   );
 }
