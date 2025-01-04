fa05a7753c573594d19757b8b03825efc041dcfe
PremPrasad <103207259+PremK01@users.noreply.github.com>
2025-01-04 12:22:59 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,6 @@
 //projects.js
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
 function Projects() {
